3110f0226406033602e0040765d9f9554e652479
Bhanumathi VM <vmbhanumathi22@gmail.com>
2022-04-14 15:49:48 +1200

    Update Test2.cs
    added changes to sql connection config, exception handling. made change to access modifier in Result class.

--- InterviewCodeReviewTest/Test2.cs
+++ InterviewCodeReviewTest/Test2.cs
@@ -8,6 +8,13 @@ namespace InterviewCodeReviewTest
 		// Record customer purchase and update customer reward programme
 		public Result UpdateCustomerHistory(Purchase customerPurchase)
 		{
+			
+			/*use a config file to configure the Database connection properties,do not hardcode in code 
+			Read connectionstring  from config file using Configuration class. 
+			using (string connStr = ConfigurationManager. ConnectionStrings["connection"])
+			{
+				opening a new sqlconnection by using "using" keyword will automatically manage the connection (closing connections)
+			}*/
 			var connPruchase = new SqlConnection("data source=TestPurchaseServer;initial catalog=PurchaseDB;Trusted_Connection=True");
 			var connReward = new SqlConnection("data source=TestRewardServer;initial catalog=RewardDB;Trusted_Connection=True");
 
@@ -19,6 +26,22 @@ namespace InterviewCodeReviewTest
 
 			try
 			{
+				/* Seoarating two try-catch blocks for sql connection opener and sql command execution will separate the 2 different exceptions.
+			or any outer exception handling for all of the sql operations with an internal exception handling for sql command execution also works
+			ex :
+			try{
+			--code for sqlconnection opener
+				try {
+				--sql querying code--
+				}
+				
+				catch(SQLExeption ex){
+				--catch actions--
+				}
+			catch(Exception ex){
+			--handling sql connection exception--
+			}
+			*/
 				connPruchase.Open();
 				tranPurchase = connPruchase.BeginTransaction();
 				cmdPurchase.ExecuteNonQuery();
@@ -49,6 +72,7 @@ namespace InterviewCodeReviewTest
 
 	public class Result
 	{
+		//IsSuccessful can be made private as teh value is not supposed to be changed externally, get & set mothods will give success & failure values.
 		public bool IsSuccessful { get; private set; }
 
 		public static Result Success()
